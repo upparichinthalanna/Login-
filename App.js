886f56bde57d7fa3dba0be3992511b546b9cfbe9
upparichinthalanna <167721332+upparichinthalanna@users.noreply.github.com>
2024-07-04 08:18:26 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App
